581c5ca405ae15e6e9b4ad66ffbd27ff59bc2e4d
Dan Johnson <danimal.johnson@gmail.com>
2019-09-16 18:33:16 -0700

    Added the 2 missing files

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,24 +1,47 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+console.log("\n\n\n"); // Add some space between the files.
 
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
 // myFunction();
 
+const myFunction = () => 
+    console.log("Function was invoked!");
+myFunction();
+
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+//anotherFunction("Example");
+
+const anotherFunction = (param) => param;
+console.log(anotherFunction("Example"));
+
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (num1, num2) => num1 + num2;
+console.log(add(1,2));
+
+
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const subtract = (num1, num2) => num1 - num2;
+console.log(subtract(1,2));
+
+
 
 
 // Stretch
@@ -28,3 +51,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+let exampleArray = [1,2,3,4];
+let triple = exampleArray.map(a => a * 3);
+console.log (triple);
